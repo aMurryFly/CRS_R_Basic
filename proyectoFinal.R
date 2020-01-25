dd34945a2195969798124f75ceaa748d05861af5
Alfonso Murrieta <54652978+aMurryFly@users.noreply.github.com>
2020-01-25 12:29:32 -0600

    Add files via upload

--- proyectoFinal.R
+++ proyectoFinal.R
@@ -55,10 +55,8 @@ HR_comma_sep %>% group_by(salary) %>% summarise(
 
 HR_comma_sep$salary <- factor(HR_comma_sep$salary,
                               levels = c("high","medium","low"))
-
 ggplot(HR_comma_sep, aes(x=salary
                          , y= satisfaction_level))+
-  
   geom_boxplot(aes(fill=salary))+
   xlab("Salario")+
   ylab("Nivel de satisfacción")+
@@ -68,6 +66,79 @@ ggplot(HR_comma_sep, aes(x=salary
     labels=c("Alto","Medio","Bajo")
   )+
   labs(title = "4. Nivel de satisfacción respecto al salario")
+
+#####Validar la satisfacción al abandonar la empresa
+class(HR_comma_sep$left)
+
+ggplot(HR_comma_sep, aes(satisfaction_level, 
+                         fill= factor(left)))+
+  geom_density(alpha=.3)+
+  xlab("Nivel de satisfacción")+
+  ylab("")+
+  coord_flip()+
+  scale_fill_manual(
+    name="Abandono",
+    values=c('lightpink1', 'lightblue1'),
+    labels=c("No","Sí")
+  )+
+  labs(title = "5. Distribución de nivel de satisfacción")
+
+ggplot(HR_comma_sep, aes(satisfaction_level, 
+                         fill= factor(Work_accident)))+
+  geom_density(alpha=.3)+
+  xlab("Nivel de satisfacción")+
+  ylab("")+
+  coord_flip()+
+  scale_fill_manual(
+    name="Accidentes de trabajo",
+    values=c('lightpink1', 'lightblue1'),
+    labels=c("No","Sí")
+  )+
+  labs(title = "6. Nivel de satisfacción por accidente de trabajo")
+
+ggplot(HR_comma_sep, aes(satisfaction_level, 
+                         fill= factor(promotion_last_5years)))+
+  geom_density(alpha=.3)+
+  xlab("Nivel de satisfacción")+
+  ylab("")+
+  coord_flip()+
+  scale_fill_manual(
+    name="Promoción en 5 años",
+    values=c('lightpink1', 'lightblue1'),
+    labels=c("No","Sí")
+  )+
+  labs(title = "7. Nivel de satisfacción por promoción")
+
+ggplot(HR_comma_sep, aes(number_project, 
+                         satisfaction_level))+
+  geom_line(aes(colour=number_project))+
+  ylab('Nivel de Satisfacción') +
+  xlab('Número de proyectos') +
+  labs(title = 'Gráfico 8: Nivel de Satisfacción por Número de Proyectos') +
+  theme(legend.position = 'none')
+
+ggplot(HR_comma_sep, aes(factor(number_project),
+                         average_montly_hours, 
+                         colour=number_project
+                         ))+
+  geom_boxplot()+
+  ylab("Promedio de Horas")+
+  xlab("Número de proyecto")+
+  labs(title = 'Gráfico 9: Número de Proyectos por Promedio 
+       de Horas al mes',
+       color = 'Número de Proyectos',
+       size = '')
+  
+  
+    
+  
+
+
+
+
+
+
+
 
 
 
